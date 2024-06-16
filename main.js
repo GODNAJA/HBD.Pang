b6abd5b6db40dfb68e5f4654c1552661bd7eb3c4
GODNAJA <105769325+GODNAJA@users.noreply.github.com>
2024-06-16 22:24:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-$(document).ready(function() {
-    $(function () {
-    var flame = $("#flame");
-    var txt = $("h1");
-
-    flame.on({
-        click: function () {
-           txt.hide().html("ขอให้แป้งใจดี").delay(750).fadeIn(300); 
-        }
-        click: function () {
-        flame.removeClass("burn").addClass("puff");
-        $(".smoke").each(function () {
-            $(this).addClass("puff-bubble");
-        });
-        $("#glow").remove();
-        txt.hide().html("ขอให้แป้งใจดีมากๆ").delay(750).fadeIn(300);
-        $("#candle").animate(
-            {
-            opacity: ".5"
-            },
-            100
-        );
-        }
+flame.on({
+    click: function () {
+       txt.hide().html("ขอให้แป้งใจดี").delay(750).fadeIn(300); 
+    }
+    click: function () {
+    flame.removeClass("burn").addClass("puff");
+    $(".smoke").each(function () {
+        $(this).addClass("puff-bubble");
     });
-});});
+    $("#glow").remove();
+    txt.hide().html("ขอให้แป้งใจดีมากๆ").delay(750).fadeIn(300);
+    $("#candle").animate(
+        {
+        opacity: ".5"
+        },
+        100
+    );
+    }
+});
